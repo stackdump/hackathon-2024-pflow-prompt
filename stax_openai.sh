4a0bc14f5e4a1f16f53530a218af43ef17353298
stackdump <myork@stackdump.com>
2024-11-24 12:10:25 -0600

    WIP

--- stax_openai.sh
+++ stax_openai.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 run_langflow() {
-  local api_url="https://api.langflow.astra.datastax.com/lf/832ceaf1-37c0-45bb-85ea-5ee73fcae7df/api/v1/run/be84ec55-d959-4b64-a6f9-0f1e46f34620?stream=false"
-  local auth_token="${DATA_STAX_KEY}" # SET via ENV variable
+  local api_url="https://api.langflow.astra.datastax.com/lf/832ceaf1-37c0-45bb-85ea-5ee73fcae7df/api/v1/run/d286a33e-4be9-47f9-9a01-f6ef0c48687f?stream=false"
+  local auth_token="${DATA_STAX_KEY_1}" # SET via ENV variable
   local input_value
   local response
 
@@ -12,16 +12,15 @@ run_langflow() {
   # Make the POST request and capture the response
   response=$(curl -s -X POST "$api_url" \
     -H 'Content-Type: application/json' \
-    -H "Authorization: Bearer $auth_token" \
     -d '{
       "input_value": "'"${input_value}"'",
-      "output_type": "chat",
-      "input_type": "chat",
-      "tweaks": {
-        "ChatInput-wZB0Q": {},
-        "OpenAIModel-O4WEP": {},
-        "ChatOutput-Y8vpL": {},
-        "TextInput-Vmm4H": {}
+    "output_type": "chat",
+    "input_type": "chat",
+    "tweaks": {
+    "ChatInput-tPBFS": {},
+    "ChatOutput-nSNHa": {},
+    "TextInput-SL8Sm": {},
+    "OpenAIModel-aVavl": {}
       }
     }')
 
